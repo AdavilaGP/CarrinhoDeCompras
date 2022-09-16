94eb6372178255f1a9994ef220b8af1f6a2deb36
AdavilaGP <adavila.andrade@gmail.com>
2022-09-16 17:14:51 -0300

    Esqueleto carrinho

---
+++ classes.py
@@ -0,0 +1,39 @@
+# Classe representando os dados do endereço do cliente
+class Endereco(BaseModel):
+    rua: str
+    cep: str
+    cidade: str
+    estado: str
+
+
+# Classe representando os dados do cliente
+class Usuario(BaseModel):
+    id: int
+    nome: str
+    email: str
+    senha: str
+
+
+from pydantic import BaseModel
+
+
+# Classe representando a lista de endereços de um cliente
+class ListaDeEnderecosDoUsuario(BaseModel):
+    usuario: Usuario
+    enderecos: List[Endereco] = []
+
+
+# Classe representando os dados do produto
+class Produto(BaseModel):
+    id: int
+    nome: str
+    descricao: str
+    preco: float
+
+
+# Classe representando o carrinho de compras de um cliente com uma lista de produtos
+class CarrinhoDeCompras(BaseModel):
+    id_usuario: int
+    id_produtos: List[Produto] = []
+    preco_total: float
+    quantidade_de_produtos: int
